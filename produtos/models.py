9222998a32f880b73966c96897e63f0c4700dc82
BrunaCardoso7 <obitadrawing@gmail.com>
2026-04-07 13:34:26 -0300

    feat: novos campos para o módulo de produtos

--- produtos/models.py
+++ produtos/models.py
@@ -13,16 +13,31 @@ class ProdutoManager(models.Manager):
 
 # Create your models here.
 class Produto(BaseModel):
+    class IeSituacaoChoice(models.TextChoices):
+        estocado = "ES", "Estocado"
+        comprar = "CP", "Comprar"
+
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         related_name="produto",
         null=True, blank=True
     )
-    nm_produto = models.CharField(max_length=255)
-    cd_barras = models.CharField(max_length=13, unique=True, db_index=True)
-    vl_venda = models.DecimalField(max_digits=10, decimal_places=2)
-    estoque = models.IntegerField(default=0)
+    ds_produto = models.CharField(max_length=255, blank=True, null=True)
+    ds_observacao = models.CharField(max_length=255, blank=True, null=True)
+    nm_fornecedor = models.CharField(max_length=255, blank=True, null=True)
+    ie_situacao = models.CharField(choices=IeSituacaoChoice.choices, max_length=2, default=IeSituacaoChoice.estocado, blank=True, null=True)
+
+    nr_estoque_minimo = models.IntegerField(blank=True, null=True)
+    nr_total_estoque = models.IntegerField(blank=True, null=True)
+
+    nr_custo_unitario = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    nr_valor_total = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    nr_valor_desconto = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    nr_valor_ajuste = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    nr_valor_imposto = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    nr_valor_frete = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    dt_validade = models.DateTimeField(null=True, blank=True)
 
     class Meta:
         db_table = "produto"
